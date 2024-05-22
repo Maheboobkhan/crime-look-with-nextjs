db7d87bc4242c23a79038b796085d5c30ec07570
Maheboobkhan <maheboobkhan18202@gmail.com>
2024-05-22 12:15:25 +0530

    removed header

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Header />
+      {/* <Header /> */}
       <Navbar />
         <main>
         {children}
